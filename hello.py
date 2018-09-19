97e69ac16b68cf91eb4ab36fca7823369bdf0835
sunyinkai <1091491336@qq.com>
2018-09-19 12:21:17 +0800

    content

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,redirect,session,url_for,flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import Form
-from wtforms import StringField,BooleanField,SubmitField
+from wtforms import StringField,BooleanField,SubmitField,PasswordField
 from wtforms.validators import Required,Email,AnyOf
 
 
@@ -9,11 +9,16 @@ app = Flask(__name__)
 bootstrap=Bootstrap(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
 class NameForm(Form): #define a form
-	name=StringField('what is your name?',validators=[Required()])
-	submit= SubmitField('Submit')
+	name=StringField('username',validators=[Required(u'please input the valid user id!')])
+	password=PasswordField('password',validators=[Required(message=u'Please input the correct password!')])
+	submit= SubmitField(u'Login')
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/')
 def index():
+	return render_template('index.html')
+
+@app.route('/login',methods=['GET','POST'])
+def login():
 	name=None
 	form=NameForm()
 	if form.validate_on_submit():
@@ -21,12 +26,8 @@ def index():
 		if odd_name is not None and odd_name != form.name.data :
 			flash("You have changed your name!")
 		session['name']=form.name.data
-		return redirect(url_for('index'))
-	return render_template('index.html',form=form,name=session.get('name'))
-
-@app.route('/user/<name>')
-def user(name):
-	return render_template('user.html',name=name)
+		return redirect(url_for('login'))
+	return render_template('/login.html',form=form,name=session.get('name'))
 
 #deal the error
 @app.errorhandler(404)
