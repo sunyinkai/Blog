1bf4ded9a15b6363aea67bed12f36431adcf7366
sunyinkai <1091491336@qq.com>
2018-09-24 15:45:39 +0800

    database

---
+++ database/debug.py
@@ -0,0 +1,3 @@
+from  flask_sqlalchemy import SQLAlchemy
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
--- hello.py
+++ hello.py
@@ -3,14 +3,41 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import Form
 from wtforms import StringField,BooleanField,SubmitField,PasswordField
 from wtforms.validators import Required,Email,AnyOf
-
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_script import Manager,Shell
+basedir =os.path.abspath(os.path.dirname(__file__)) #the database
 
 app = Flask(__name__)
+manager = Manager(app)
+app.config['SQLALCHEMY_DATABASE_URI']=\
+'sqlite:///' + os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
+db=SQLAlchemy(app) # create an SQLAlchemy Object
+
+class Role(db.Model):
+	__tablename__ ='roles'
+	id=db.Column(db.Integer,primary_key=True)
+	name=db.Column(db.String(64),unique=True)
+	users = db.relationship('User',backref='role')
+	def __repr__(self):
+		return '<Role %r>' %self.name
+
+
+class User(db.Model):
+	__tablename__='users'
+	id=db.Column(db.Integer,primary_key=True)
+	username=db.Column(db.String(64),unique=True,index=True)
+	role_id= db.Column(db.Integer,db.ForeignKey('roles.id'))
+	def __repr__(self):
+		return '<User %r>' % self.username
+
 bootstrap=Bootstrap(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
 class NameForm(Form): #define a form
 	name=StringField('username',validators=[Required(u'please input the valid user id!')])
 	password=PasswordField('password',validators=[Required(message=u'Please input the correct password!')])
+	remeber_me=BooleanField('remerber me',default=False)
 	submit= SubmitField(u'Login')
 
 @app.route('/')
@@ -22,12 +49,22 @@ def login():
 	name=None
 	form=NameForm()
 	if form.validate_on_submit():
-		odd_name=session.get('name')
-		if odd_name is not None and odd_name != form.name.data :
-			flash("You have changed your name!")
+		user = User.query.filter_by(username=form.name.data).first()
+		if user is None :
+			user = User(username = form.name.data)
+			db.session.add(user)
+			db.session.commit()
+			session['known']=False
+		else:
+			session['known']=True
 		session['name']=form.name.data
+		form.name.data=''
+		odd_name=session.get('name')
+		'''if odd_name is not None and odd_name != form.name.data :
+			flash("You have changed your name!")
+		session['name']=form.name.data'''
 		return redirect(url_for('login'))
-	return render_template('/login.html',form=form,name=session.get('name'))
+	return render_template('/login.html',form=form,name=session.get('name'),known = session.get('known',False))
 
 #deal the error
 @app.errorhandler(404)
@@ -36,5 +73,10 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
 	return render_template('500.html')
+
+def make_shell_context():
+	return dict(app=app,db=db,User=User,Role=Role)
 if  __name__ == '__main__':
+	#manager.add_command("shell",Shell(make_context=make_shell_context))
+	#manager.run()
     app.run(host='0.0.0.0',port=5000)
