082a408d2430dd9942f4c4cffa27577bdd6b4ed5
sunyinkai <1091491336@qq.com>
2018-12-17 09:20:05 +0800

    12_17 9:20

--- app/search/views.py
+++ app/search/views.py
@@ -1,15 +1,19 @@
-from flask import render_template, redirect, request, url_for, flash
+from flask import render_template, redirect, request, url_for, flash,current_app
 from flask_login import login_required, current_user
 from sqlalchemy import or_
 from .. import db
 from . import search
-from ..models import Post
+from ..models import Post,User
 
 
 @search.route('/search')
 def search():
     keyword = request.args.get('keyword')
-    posts = Post.query.filter(or_(Post.title.contains(keyword),
-                                  Post.body.contains(keyword))).order_by(Post.timestamp.desc()).all()
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.filter(or_(Post.title.contains(keyword),
+                                  Post.body.contains(keyword))).order_by(Post.timestamp.desc()).paginate(
+        page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'], error_out=False
+    )
+    posts = pagination.items
     return render_template('search/result.html',
-                           post_set=posts)  # this place is is post_set -> posts will be wrong!!why
+                           post_set=posts, pagination=pagination)
